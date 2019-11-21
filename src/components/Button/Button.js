c9415453ac47cfdb88df8d574969eb4dd6c640b6
Michelle <michelle.pine33@gmail.com>
2019-11-21 12:32:42 -0500

    add packages and plop files, first component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.scss';
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+    <div>
+    </div>
+    );
+  }
+};
+
+Button.defaultProps = {
+
+};
+
+Button.propTypes = {
+
+};
+
+export default Button;

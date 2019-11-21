69fb27f972ab7a8892eda87c9ed7326baf03a356
Michelle <michelle.pine33@gmail.com>
2019-11-21 12:47:58 -0500

    set up stores, change to sass

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { Route, HashRouter as Router } from 'react-router-dom'
+
+import './index.scss';
+//stores
+import store from './store/index'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Provider store={store}>
+    <div className="page-wrapper">
+      <Router basename='/'>
+        <div>
+          <Route exact path="/" component={App} />
+        </div>
+      </Router>
+    </div>
+  </Provider>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+
+function getInitialState() {
+
+  const initialState = {
+  };
+
+  return initialState;
+}
+
+function rootReducer(state = getInitialState(), action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;

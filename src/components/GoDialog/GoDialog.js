873801eeac488d2a3cfb3787774ae5c124228491
Michelle <michelle.pine33@gmail.com>
2019-12-08 17:46:34 -0500

    fix

--- src/components/GoDialog/GoDialog.js
+++ src/components/GoDialog/GoDialog.js
@@ -28,9 +28,6 @@ class GoDialog extends React.Component {
         this.setState({loaderShow: false});
         alert("You don't have geolocation services enabled. Enable them before continuing.")
       }.bind(this));
-    } else {
-      this.setState({loaderShow: false});
-      alert("You don't have geolocation services enabled. Enable them before continuing.");
     }
   }
 
